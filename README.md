bbe54e0ed1b0215c2aad0c826b4a37bf363f8d31
seancroach <me@seancroach.dev>
2021-05-04 11:21:33 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# `docgen` &nbsp;[![Build Status]][actions] [![Latest Version]][crates.io]
+
+[Build Status]: https://img.shields.io/github/workflow/status/residua/docgen/CI?logo=github
+[actions]: https://github.com/residua/docgen/actions/workflows/ci.yml
+[Latest Version]: https://img.shields.io/crates/v/docgen128?logo=rust
+[crates.io]: https://crates.io/crates/docgen128
+
+*Rust library for reading and writing unsigned integers in LEB128 encoding.* View the documentation on `docs.rs` [here][docs].
+
+[docs]: https://docs.rs/docgen128
+
+## License
+
+This crate is available as open source under the terms of the [MIT License](https://github.com/residua/docgen/blob/latest/LICENSE.md).
---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+//! This small crate is to let developers easily document items generated in
+//! macros where the content of the documentation is dynamic.
+//!
+//! # Examples
+//!
+//! ```
+//! #[macro_use]
+//! extern crate doc;
+//!
+//! doc!(
+//!     "Here is some documentation!"
+//!     ""
+//!     "Empty lines are represented by empty strings.";
+//!     pub fn foo() {}
+//! );
+//!
+//! # fn main() {}
+//! ```
+//!
+//! Commas can be used as delimitter between lines:
+//!
+//! ```
+//! #[macro_use]
+//! extern crate doc;
+//!
+//! doc!(
+//!     "Here is some documentation!",
+//!     "",
+//!     "Empty lines are represented by empty strings.";
+//!     pub fn foo() {}
+//! );
+//! # fn main() {}
+//! ```
+//!
+//! This is particularly useful when documenting
+//!
+//! ```
+//! #[macro_use]
+//! extern crate doc;
+//!
+//! macro_rules! add_fn {
+//!     ($name:ident, $ty:ty) => {
+//!         doc!(
+//!             concat!("Add two [`", stringify!($ty), "`] values together.");
+//!             pub fn $name(a: $ty, b: $ty) -> $ty {
+//!                 a + b
+//!             }
+//!         );
+//!     }
+//! }
+//!
+//! add_fn!(add_u8, u8);
+//! add_fn!(add_i8, i8);
+//! # fn main() {}
+//! ```
+
+/// This macro is used to generate documentation upon an item.
+///
+/// # Examples
+///
+/// ```
+/// #[macro_use]
+/// extern crate doc;
+///
+/// doc!(
+///     "Here is the documentation!"
+///     ""
+///     "Tah dah!";
+///     #[inline]
+///     pub fn foo() {}
+/// );
+///
+/// # fn main() {}
+/// ```
+#[macro_export]
+macro_rules! doc {
+    ($($expr:expr)*; $x:item) => {
+        $(#[doc = $expr])*
+        $x
+    };
+    // The same as the case above, but commas between expressions are allowed.
+    ($($expr:expr),*; $x:item) => {
+        $(#[doc = $expr])*
+        $x
+    };
+}
